51c81f43f2f6d8a135ce87e11bbfa02ecb135304
Nadya Novichkova <nadya.nov13@gmail.com>
2025-07-30 21:08:27 -0700

    this is another copy of the stack written in a minute

---
+++ stack/N_Stack_23.h
@@ -0,0 +1,26 @@
+#include "node.h"
+#include <iostream>
+
+class Stack{
+    Node* top;
+
+    bool isEmpty(){
+        return top == NULL;
+    }
+    int peek(){
+        return top->val;
+    }
+    int remove(){
+        Node* topNode = top;
+        int output = top->val;
+        top = top->next;
+        delete topNode;
+        return output;        
+    }
+    void add(int value){
+        Node* newNode = new Node(value);
+        top->next = newNode;
+        top = newNode;
+    }
+
+};
