a429885ad96203e3c24e01ffedee21848f78aff8
Nadya Novichkova <nadya.nov13@gmail.com>
2025-07-31 22:12:18 -0700

    another practice of binary search

---
+++ binarySearch/N_binarySearch_07.h
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class ArrrayUtils{
+    private:
+    public:
+
+    int binarySearch(int arr[], int value, int size){
+        int left = 0;
+        int right = size -1;
+        int mid;
+        while(left < right){
+            mid = (right + left)/2;
+            if(arr[mid] == value){
+                return mid;
+            }
+            else if (value < arr[mid]){
+                right = mid;
+            }
+            else if(value > arr[mid]){
+                left = mid;
+            }
+
+        }
+        return -1;
+
+
+    }
+};
