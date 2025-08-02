180e8e5e3f3f53ecd124f9f0da4b35ea8f0e1581
Nadya Novichkova <nadya.nov13@gmail.com>
2025-08-01 21:37:04 -0700

    this is the first practice copy of tree travesals

---
+++ tree/N_TreeTraversals_01.h
@@ -0,0 +1,53 @@
+#include "tree.h"
+#include <queue>
+
+class TreeTraverals{
+    public:
+    void pre_Order(TreeNode* node){
+         if(node == NULL){
+            return;
+         }
+         node->visit();
+         pre_Order(node->left);
+         pre_Order(node->right);
+    }
+    void in_Order(TreeNode* node){
+        if(node == NULL){
+            return;
+        }
+        in_Order(node->left);
+        node->visit();
+        in_Order(node->right);
+    }
+    void post_Order(TreeNode* node){
+        if(node == NULL){
+            return;
+        }
+        post_Order(node->left);
+        post_Order(node->right);
+        node->visit();
+    }
+    void level_Order(TreeNode* node){
+        queue<TreeNode*> q;
+
+        if(node != NULL){
+            q.push(node);
+        }
+        
+        while(!q.empty()){
+            TreeNode* topNode = q.front();
+            topNode->visit();
+
+            if(node->left != NULL){
+                q.push(topNode->left);   
+            }
+            if(node->right != NULL){
+                q.push(topNode->right);
+            }
+
+            q.pop();
+        }
+
+    }
+
+};
