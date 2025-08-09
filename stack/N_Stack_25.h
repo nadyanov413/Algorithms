343bdf4870ea990be42415c62991c39fad6a1acb
Nadya Novichkova <nadya.nov13@gmail.com>
2025-08-08 22:23:15 -0700

    this is another stack file

---
+++ stack/N_Stack_25.h
@@ -0,0 +1,25 @@
+#include "node.h"
+#include <iostream>
+
+class Stack{
+    Node* top;
+
+    bool isEmpty(){
+        return top == NULL;
+    }
+    int peek(){
+        return top->val;
+    }
+    int remove(){
+        Node* topNode = top;
+        int value = top->val;
+        top = top->next;
+        delete topNode;
+        return value;
+    }
+    void add(int value){
+        Node* newNode = new Node(value);
+        top->next = newNode;
+        top = newNode;
+    }
+};
